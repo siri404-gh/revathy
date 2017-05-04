ada0df69bde9c2f5adffc051be43a25d062416de
Sreeram Padmanabhan <sreeram.com@gmail.com>
2017-05-04 02:18:53 +0100

    Responsiveness

--- js/myscript.js
+++ js/myscript.js
@@ -1,16 +1,10 @@
-var elements = ['websiteTitle', 'websiteTag', 'heroText', 'heroDescription'];
-var title = 'Nattamai';
-var values = [title, 'The one in a million', 'I am Revathy', 'And I\'m a Web Developer'];
 window.onload = function() {
-    document.title = title;
-    elements.map(function(e, i) {
-        document.getElementById(e).innerHTML = values[i];
-    });
     var links = document.getElementsByClassName('link');
+    
     for (var i = 0; i< links.length; i++) {
         links[i].addEventListener('click', function() {
             var id = this.innerHTML.toLowerCase();
-            var top = $("#"+id).offset().top - 75;
+            var top = $("#"+id).offset().top - 50;
             $('html, body').animate({
                 scrollTop: top
             }, 500);
